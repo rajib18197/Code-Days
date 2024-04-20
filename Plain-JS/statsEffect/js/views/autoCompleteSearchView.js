8bac6fb509f91afe0af7bdc1ae5ef18e4344186e
rajib18197 <rajib18197@gmail.com>
2024-04-20 21:10:30 +0600

    re-factor the whole codebase for better code organization

--- Plain-JS/statsEffect/js/views/autoCompleteSearchView.js
+++ Plain-JS/statsEffect/js/views/autoCompleteSearchView.js
@@ -1,109 +1,56 @@
 import { debounce } from "../utils/helpers.js";
 
 export default class AutoCompleteSearchView {
-  data;
+  #data;
 
-  constructor({
-    rootElement,
-    apiService,
-    renderOptionMarkup,
-    itemDatasetValue,
-    inputValue,
-    setSelectedOptions,
-    onOptionSelection,
-  }) {
-    this.data = {
+  constructor({ rootElement, apiService }) {
+    this.#data = {
       rootElement,
       apiService,
-      renderOptionMarkup,
-      itemDatasetValue,
-      inputValue,
-      setSelectedOptions,
-      onOptionSelection,
     };
 
-    const markup = this.#generateAutoCompleteMarkup();
-    this.data.rootElement.insertAdjacentHTML("beforeend", markup);
-
-    this.addHandlerInputChange();
-    this.closeDropdownOutside();
+    const markup = this.#generateInitialMarkup();
+    this.#clear();
+    this.#data.rootElement.insertAdjacentHTML("beforeend", markup);
+    this.#addHandlerInputChange();
   }
 
-  showDropdown() {
-    this.data.rootElement
-      .querySelector(".dropdown")
-      .classList.remove("dropdown__hidden");
+  #clear() {
+    this.#data.rootElement.innerHTML = "";
   }
 
-  closeDropdown() {
-    this.data.rootElement
-      .querySelector(".dropdown")
-      .classList.add("dropdown__hidden");
+  async #onInputChange(e) {
+    const data = await this.#data.apiService({ searchTerm: e.target.value });
+    this.#generateDropdownItemMarkup(data);
   }
 
-  closeDropdownOutside() {
-    document.addEventListener("click", (e) => {
-      if (!e.target.contains(this.data.rootElement)) {
-        this.closeDropdown();
-      }
-    });
-  }
-
-  async #handleInputChange(e) {
-    const data = await this.data.apiService(e.target.value);
-    this.showDropdown();
-
-    const selectedOptions = {};
-
-    const optionMarkup = data
-      .map((item) => {
-        const optionObj = this.data.setSelectedOptions(item);
-        selectedOptions[optionObj.key] = optionObj.value;
-
-        const value = this.data.itemDatasetValue(item);
-        const optionMarkup = this.data.renderOptionMarkup(item);
-        const markup = `
-      	<div class='dropdown__item' data-value="${value}">${optionMarkup}</div>
-			`;
-        return markup;
-      })
-      .join("");
-
-    this.data.rootElement
-      .querySelector(".dropdown__menu")
-      .insertAdjacentHTML("beforeend", optionMarkup);
-
-    this.data.rootElement.addEventListener("click", (e) => {
-      if (e.target.closest(".dropdown__item")) {
-        this.closeDropdown();
-        const selectedValue = e.target.closest(".dropdown__item").dataset.value;
-
-        this.data.rootElement.querySelector("#search").value =
-          selectedOptions[selectedValue];
-
-        this.data.onOptionSelection(selectedValue);
-      }
-    });
-  }
-
-  addHandlerInputChange() {
-    const inputElement = this.data.rootElement.querySelector("#search");
-    inputElement.addEventListener(
+  #addHandlerInputChange() {
+    this.#data.rootElement.addEventListener(
       "input",
-      debounce(this.#handleInputChange.bind(this), 1000)
+      debounce(this.#onInputChange.bind(this), 1000)
     );
   }
 
-  #generateAutoCompleteMarkup() {
-    return `
-			<label for="search">Search Your Thing</label>
-			<input type="text" id="search" />
+  #generateDropdownItemMarkup(data) {
+    return `${data.map(
+      (item) => `
+      <div class="dropdown__item">
+        ${this.#data.renderDropDownContentMarkup(item)}
+      </div>
+    `
+    )}
+    `;
+  }
 
-			<div class="dropdown dropdown__hidden">
-				<div class="dropdown__menu">
-					
-				</div>
-			</div>
-		`;
+  #generateInitialMarkup() {
+    return `
+      <label for="search">Search Your Thing...</label>
+      <input type="text" id="search" />
+      <div class="dropdown">
+        <div class="dropdown__menu">
+          
+        </div>
+      </div>
+    `;
   }
 }
