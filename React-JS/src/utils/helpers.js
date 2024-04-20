bd6fd7988df34a88a08437e731f5efe65060cdd8
rajib18197 <rajib18197@gmail.com>
2024-04-20 22:24:59 +0600

    implemented a split function which split a string by a divider

--- React-JS/src/utils/helpers.js
+++ React-JS/src/utils/helpers.js
@@ -8,3 +8,27 @@ export const hasPointedHalf = function (e) {
 
   return false;
 };
+
+const split = function (str, divider) {
+  const arr = [];
+  let word = "";
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] === divider) {
+      arr[arr.length] = word;
+      word = "";
+      continue;
+    }
+
+    word += str[i];
+  }
+
+  if (word) arr[arr.length] = word;
+
+  console.log(arr);
+  return arr;
+};
+
+const transform = split("a+very+nice+string", "+");
+const transform2 = split("Ra Ju", " ");
+
+console.log(transform, transform2);
