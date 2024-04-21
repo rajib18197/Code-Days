55bcf6b32b6b311b1ccea5061721bc070d85af60
rajib18197 <rajib18197@gmail.com>
2024-04-21 22:27:52 +0600

    implemented config based UI

--- React-JS/src/features/fight/moviesFight.jsx
+++ React-JS/src/features/fight/moviesFight.jsx
@@ -5,6 +5,18 @@ import AutoComplete from "../../ui/AutoComplete";
 export default function MoviesFight() {
   const [movieDetails, setMovieDetails] = useState({});
 
+  const config = {
+    onSearch(searchTerm) {
+      getMoviesData({ searchTerm });
+    },
+    onSelection(movie) {
+      handleSelection(movie);
+    },
+    onInputValue(movie) {
+      return movie.Title;
+    },
+  };
+
   async function handleSelection(movie) {
     const movieDetails = await getMoviesData({ imdbID: movie.imdbID });
     setMovieDetails(movieDetails);
@@ -14,35 +26,37 @@ export default function MoviesFight() {
     <div>
       <header className="header"></header>
       <div className="columns">
-        <div className="column">
-          <div className="left-autocomplete">
-            <AutoComplete
-              onSearch={(searchTerm) => getMoviesData({ searchTerm })}
-              onSelection={handleSelection}
-              onInputValue={(movie) => movie.Title}
-            />
+        {Array.from({ length: 2 }, (_, i) => (
+          <div className="column">
+            <div className="left-autocomplete">
+              <AutoComplete
+                onSearch={config.onSearch}
+                onSelection={config.onSelection}
+                onInputValue={config.onInputValue}
+              />
+            </div>
+            <div className="left-summary">
+              {Object.keys(movieDetails).length > 0 && (
+                <>
+                  <img src={`${movieDetails.Poster}`} alt="movie" />
+                  <p>{movieDetails.Title}</p>
+                  <p>{movieDetails.Plot}</p>
+                  <p>
+                    Imdb Rating: {movieDetails.imdbRating}, Release Year:
+                    {movieDetails.Year}
+                  </p>
+                  <p>
+                    Actors: {movieDetails.Actors}, Director:
+                    {movieDetails.Director}
+                  </p>
+                  <p>Awards: {movieDetails.Awards}</p>
+                  <p>Income: {movieDetails.BoxOffice}</p>
+                  <p>Votes: {movieDetails.imdbVotes}</p>
+                </>
+              )}
+            </div>
           </div>
-          <div className="left-summary">
-            {Object.keys(movieDetails).length > 0 && (
-              <>
-                <img src={`${movieDetails.Poster}`} alt="movie" />
-                <p>{movieDetails.Title}</p>
-                <p>{movieDetails.Plot}</p>
-                <p>
-                  Imdb Rating: {movieDetails.imdbRating}, Release Year:
-                  {movieDetails.Year}
-                </p>
-                <p>
-                  Actors: {movieDetails.Actors}, Director:
-                  {movieDetails.Director}
-                </p>
-                <p>Awards: {movieDetails.Awards}</p>
-                <p>Income: {movieDetails.BoxOffice}</p>
-                <p>Votes: {movieDetails.imdbVotes}</p>
-              </>
-            )}
-          </div>
-        </div>
+        ))}
 
         <div className="column">
           <div className="right-autocomplete"></div>
