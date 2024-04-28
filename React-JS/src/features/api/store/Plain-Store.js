80de6b35fd86f092ab005ffcddb07fbe1250738e
rajib18197 <rajib18197@gmail.com>
2024-04-28 14:09:22 +0600

    refactor to class based implementation

--- React-JS/src/features/api/store/Plain-Store.js
+++ React-JS/src/features/api/store/Plain-Store.js
@@ -1,44 +1,46 @@
-const combineReducers = function (obj, { state, action = {} } = {}) {
-  const appState = {};
+class Redux {
+  static combineReducers(obj, { state, action = {} } = {}) {
+    const appState = {};
 
-  for (let [key, value] of Object.entries(obj)) {
-    appState[key] = obj[key](state?.[key], action);
+    for (let [key, value] of Object.entries(obj)) {
+      appState[key] = obj[key](state?.[key], action);
+    }
+
+    return { obj, appState };
   }
 
-  return { obj, appState };
-};
+  static createStore(reducer, initialState) {
+    const store = {
+      state: reducer.appState,
+      listeners: [],
+      getState() {
+        return this.state;
+      },
+    };
 
-const createStore = function (reducer, initialState) {
-  const store = {
-    state: reducer.appState,
-    listeners: [],
-    getState() {
-      return this.state;
-    },
-  };
+    store.dispatch = function (action) {
+      // Action must be an plain JS object (TODO: implement later)
 
-  store.dispatch = function (action) {
-    // Action must be an plain JS object (TODO: implement later)
-    const newState = combineReducers(reducer.obj, {
-      state: this.state,
-      action,
-    });
-    // console.log(newState);
+      const newState = Redux.combineReducers(reducer.obj, {
+        state: this.state,
+        action,
+      });
 
-    if (newState !== this.state) {
-      this.state = newState;
-      this.listeners.forEach((listener) => listener());
-    }
-  };
+      if (newState !== this.state) {
+        this.state = newState.appState;
+        this.listeners.forEach((listener) => listener());
+      }
+    };
 
-  store.subscribe = function (listener) {
-    this.listeners.push(listener);
-  };
+    store.subscribe = function (listener) {
+      this.listeners.push(listener);
+    };
 
-  return store;
-};
+    return store;
+  }
+}
 
-const rootReducer = combineReducers({
+const rootReducer = Redux.combineReducers({
   cart: (state = [], action) => {
     if (action.type === "CART/ADD_NEW_ITEM") {
       return [...state, action.payload];
@@ -49,18 +51,33 @@ const rootReducer = combineReducers({
 
   menus: (state = [], action) => {
     if (action.type === "MENUS/LOADED") {
-      return [...state, action.payload];
+      return [...action.payload];
     }
 
     return state;
   },
 });
 
-const store = createStore(rootReducer);
+const store = Redux.createStore(rootReducer);
 
 store.dispatch({
   type: "CART/ADD_NEW_ITEM",
-  payload: { id: 1, name: "Pizza Avocado", price: 22 },
+  payload: { id: 1, name: "Pizza Avocado", unitPrice: 22, quantity: 2 },
+});
+
+store.dispatch({
+  type: "CART/ADD_NEW_ITEM",
+  payload: { id: 2, name: "Vege Brocolli", unitPrice: 12, quantity: 1 },
+});
+
+store.dispatch({
+  type: "MENUS/LOADED",
+  payload: [
+    { id: 1, name: "Pizza Avocado", unitPrice: 22 },
+    { id: 2, name: "Vege Brocolli", unitPrice: 12 },
+    { id: 3, name: "Spinnach Caserolle", unitPrice: 12 },
+    { id: 4, name: "Italian Pizzaria", unitPrice: 12 },
+  ],
 });
 
 console.log(store.getState());
