c557a5aabc5fb13fd260db947b8a8d3592788682
rajib18197 <rajib18197@gmail.com>
2024-07-29 20:38:13 +0600

    practice some JS related stuff

--- Plain-JS/problem-solving/tree.js
+++ Plain-JS/problem-solving/tree.js
@@ -463,3 +463,224 @@ console.log(max);
   6, -1, 42, 1, 36, -1, -1, 7, -1, 22, 41, -1, 4, 48, 32, -1, -1, 14, 21, -1,
   39, 8, -1, -1, -1, -1, -1, -1, -1, -1, -1,
 ];
+
+// *****************
+
+// document.addEventListener("DOMContentLoaded", function () {
+//   console.log("DOM");
+// });
+
+// const power = function (a, b) {
+//   let temp = 0;
+
+//   if (b === 0) return 1;
+
+//   temp = power(a, Math.floor(b / 2));
+//   console.log(temp);
+
+//   if (b % 2 === 0) return temp * temp;
+//   else return a * temp * temp;
+// };
+
+// const power1 = power(2, 6);
+// console.log(power1);
+
+const argsCount = function (number) {
+  let count = 0;
+  let num = number;
+  let sum = 0;
+  while (num !== 0) {
+    sum = sum + (num % 10);
+    num = Math.floor(num / 10);
+    // console.log(sum);
+    // console.log(num);
+    count++;
+  }
+  console.log(count);
+};
+
+argsCount(5684);
+argsCount(155689);
+argsCount(9867295);
+
+// In computing, a process is the instance of a computer program that is being executed by one or many threads. It contains the program code and its activity. Depending on the operating system (OS), a process may be made up of multiple threads of execution that execute instructions concurrently
+
+// A system is said to be concurrent if it can support two or more actions in progress at the same time. A system is said to be parallel if it can support two or more actions executing simultaneously.
+// JavaScript is weird and powerful at the same time.
+
+// It is a JIT programming language.
+
+// It is first compiled then executed.
+
+// But it can be compiled and executed at the same time!
+
+// That is what JIT means: just in time compiled.
+
+// You must set youreslf a correct mental model about this if you want to be good at JS.
+
+// The compilation means that, the JS parser reads the code and saves it in memory. Here we talk about instances.
+
+// Then it executes it when it is called in our program.
+
+// All of this may happen at 2 different times or at the same time.
+
+// It's a harsh mental model to grab.
+/* Data used in exercises */
+const books = [
+  {
+    title: "The Lord of the Rings",
+    publicationDate: "1954-07-29",
+    author: "J. R. R. Tolkien",
+    genres: ["fantasy", "high-fantasy", "adventure"],
+    filmAdaptation: true,
+  },
+  {
+    title: "The Cyberiad",
+    publicationDate: 1965,
+    author: "Stanislaw Lem",
+    genres: ["science fiction"],
+  },
+  {
+    title: "Dune",
+    publicationDate: 1965,
+    author: "Frank Herbert",
+    genres: ["science fiction", "novel", "adventure"],
+    filmAdaptation: true,
+  },
+  {
+    title: "Harry Potter and the Philosopher's Stone",
+    publicationDate: "1997-06-26",
+    author: "J. K. Rowling",
+    genres: ["fantasy", "adventure"],
+    filmAdaptation: true,
+  },
+  {
+    title: "A Game of Thrones",
+    publicationDate: "1996-08-01",
+    author: "George R. R. Martin",
+    genres: ["fantasy", "high-fantasy", "novel", "fantasy fiction"],
+    filmAdaptation: true,
+  },
+];
+
+/* ⚠️ YOU WILL CALL THE FUNCTIONS BELOW IN EXERCISES.
+     DON'T WORRY IF THEY DON'T MAKE SENSE FOR NOW.
+     YOU WILL LEARN EVERYTHING A BIT LATER IN THE COURSE.
+     FOR NOW TREAT THEM AS BLACK BOXES (focus on the values they return).
+     YOU CAN CALL THEM AND LOG THE RETURNED VALUE TO THE CONSOLE TO SEE WHAT EXACTLY THEY RETURN. */
+
+const getBooksByGenre = (genre) =>
+  books.filter((book) => book.genres.includes(genre));
+const getBooksAsArrays = () => books.map((book) => Object.entries(book));
+const getBookAuthors = () => books.map((book) => book.author);
+
+/*
+ *  ********************************************
+ *  1) DESTRUCTURING ARRAYS                    *
+ *  ********************************************
+ */
+
+/* A) Destructure the 'books' array into four variables called 'a', 'b', 'c' and 'd'.
+      Leave the rest of the books unused. */
+
+const [a, b, c, d] = books;
+// console.log(a, b, c, d);
+
+/* B) The second and third books are science fiction.
+      Assign these books to the variables called 'second' and 'third' using destructuring. */
+
+/* C) The getBooksByGenre() function returns an array of books based on the genre you pass as the argument.
+      Use it to get all 'fantasy' books. Destructure the returned array into two variables — the first one called 'theLordOfTheRings',
+      and the second one called 'otherFantasyBooks' (an array containing all other values from the returned array). */
+
+const [theLordOfTheRings, ...otherFantasyBooks] = getBooksByGenre("fantasy");
+// console.log(arr);
+
+/* D) Are you ready for some hard-core destructuring? 😄
+      The getBooksAsArrays() function returns the books array, but all book objects and their properties were converted to arrays.
+      Now, you have an array of arrays of arrays.
+      Destructure the title of the second book (The Cyberiad) into a variable called 'title'. */
+
+// const arr2 = getBooksAsArrays();
+// console.log(arr2);
+// const [, [[, title]]] = arr2;
+// console.log(title);
+
+/* ⚠️ COMMENT OUT YOUR SOLUTIONS AFTER YOU FINISH SO THAT IT DOESN'T COLLIDE WITH NEXT EXERCISES 🠕 */
+
+/*
+ *  ********************************************
+ *  2) DESTRUCTURING OBJECTS                   *
+ *  ********************************************
+ */
+
+/* A) Take the first object from the 'books' array, and assign the author to the 'author' variable using destructuring.
+ *     Use the 'let' statement because the 'author' variable may change later. */
+
+/* B) Take the second object from the 'books' array, and destructure the title into a variable called 'bookTitle'. */
+
+/* C) The book objects aren't consistent in their form.
+      For example, the second book doesn't have the 'filmAdaptation' property.
+      Destructure it into a variable called 'hasFilmAdaptation' with a default value of false. */
+
+/* D) Remember the 'author' variable from exercise A? It's time to reassign it.
+      Destructure the author of the third book into existing variable called 'author'. */
+
+/* ⚠️ COMMENT OUT YOUR SOLUTIONS AFTER YOU FINISH SO THAT IT DOESN'T COLLIDE WITH NEXT EXERCISES 🠕 */
+
+/*
+ *  ********************************************
+ *  3) THE SPREAD SYNTAX                       *
+ *  ********************************************
+ */
+
+/* A) The getBookAuthors() function returns an array of authors from the 'books' array.
+      Reassign the 'authors' variable below so that it contains both — already existing authors,
+      and authors returned from the getBookAuthors() function. Use the spread syntax. */
+let authors = ["George Orwell", "Aldous Huxley"];
+
+/* B) The console.log() method can take multiple arguments and log them to the console.
+      First, log the 'authors' array as it is (as one argument).
+      Second, log the elements of the 'authors' array, but this time use the spread syntax.
+      Compare the outputs. */
+
+/* C) The spread syntax can be used with other iterables, for example, strings.
+      Create a new variable called 'firstNameArray', and spread the 'firstName' string
+      so that each letter becomes an element of the 'firstNameArray' like ['J', 'o', 'h', 'n']. */
+const firstName = "John";
+
+/* D) Now it's time to spread some objects. Create a new variable called 'cyberiad',
+      and assign an object to it. This object should have all the properties of the second book from the 'books' array,
+      plus the missing 'filmAdaptation' property set to false. */
+
+/* ⚠️ COMMENT OUT YOUR SOLUTIONS AFTER YOU FINISH SO THAT IT DOESN'T COLLIDE WITH NEXT EXERCISES 🠕 */
+const question = new Map([
+  ["name", "What is your favourite programming language?"],
+  [1, "C"],
+  [2, "Java"],
+  [3, "JavaScript"],
+  ["correct", 3],
+  [true, "Correct Answer."],
+  [false, "Try Again!"],
+]);
+
+console.log(question.set("favLine", "Be OBSESSED with LEARNING"));
+
+console.log(question.get("name"));
+for (const [key, value] of question) {
+  if (typeof key === "number") {
+    console.log(`Answer ${key}: ${value}`);
+  }
+}
+
+const answer = 3;
+
+console.log(question.get(question.get("correct") === answer));
+console.log([...question]);
+
+const obj = {
+  age: 23,
+};
+
+const r = new Map(Object.entries(obj));
+console.log(r);
