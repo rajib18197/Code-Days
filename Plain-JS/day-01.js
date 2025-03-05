b245c0ce7353941d6d35eaa40a2842d9e452056d
rajib18197 <rajib18197@gmail.com>
2025-03-05 21:46:37 +0600

    problems of graph, priority queue & minimum spanning tree

--- Plain-JS/day-01.js
+++ Plain-JS/day-01.js
@@ -69,4 +69,135 @@ const init = function () {
   maxPriceTabulation(wt, val, 5);
 };
 
-init();
+// init();
+
+class PriorityQueue {
+  constructor(totalLength) {
+    this.arr = Array.from({ length: totalLength }, () => []);
+    this.totalLength = totalLength;
+    this.curr = 0;
+  }
+
+  push(value) {
+    if (this.curr === this.totalLength) {
+      return;
+    }
+
+    this.arr[this.curr] = [...value];
+    let index = this.curr;
+    this.curr++;
+
+    let child = index;
+    let parent = Math.floor((child - 1) / 2);
+
+    while (parent >= 0 && this.arr[child][0] < this.arr[parent][0]) {
+      let temp = this.arr[child];
+      this.arr[child] = this.arr[parent];
+      this.arr[parent] = temp;
+
+      child = parent;
+      parent = Math.floor((child - 1) / 2);
+    }
+  }
+
+  remove() {
+    if (this.curr === 0) {
+      return -1;
+    }
+
+    const removedValue = this.arr[0];
+
+    this.arr[0] = this.arr[this.curr - 1];
+    this.curr--;
+
+    if (this.curr === 0) {
+      return removedValue;
+    }
+
+    let index = 0;
+    let smallest = index;
+    let left = smallest * 2 + 1;
+    let right = smallest * 2 + 2;
+
+    while (
+      (left < this.curr && this.arr[smallest][0] > this.arr[left][0]) ||
+      (right < this.curr && this.arr[smallest][0] > this.arr[right][0])
+    ) {
+      if (left < this.curr && this.arr[smallest][0] > this.arr[left][0]) {
+        smallest = left;
+      }
+
+      if (right < this.curr && this.arr[smallest][0] > this.arr[right][0]) {
+        smallest = right;
+      }
+
+      let temp = this.arr[smallest];
+      this.arr[smallest] = this.arr[index];
+      this.arr[index] = temp;
+
+      index = smallest;
+      left = smallest * 2 + 1;
+      right = smallest * 2 + 2;
+    }
+
+    return removedValue;
+  }
+}
+
+const createGraph = function (V, edges) {
+  const adj = Array.from({ length: V }, () => []);
+
+  for (let i = 0; i < edges.length; i++) {
+    const [u, v, weight] = edges[i];
+    adj[u].push([v, weight]);
+    adj[v].push([u, weight]);
+  }
+
+  return adj;
+};
+
+const calcMinimumSpanningTree = function (V, edges) {
+  const graph = createGraph(V, edges);
+
+  const pq = new PriorityQueue(edges.length);
+  const visited = Array.from({ length: V }, () => 0);
+
+  pq.push([0, 0, -1]);
+  let mstSum = 0;
+  const mst = [];
+
+  while (pq.curr !== 0) {
+    const [weight, node, parent] = pq.remove();
+
+    if (visited[node] === 1) {
+      continue;
+    }
+
+    visited[node] = 1;
+
+    if (parent !== -1) {
+      mstSum += weight;
+      mst.push([node, parent, weight]);
+    }
+
+    for (const [neighbourNode, weight] of graph[node]) {
+      if (!visited[neighbourNode]) {
+        pq.push([weight, neighbourNode, node]);
+      }
+    }
+  }
+
+  console.log(mstSum);
+  console.log(mst);
+
+  return { mstEdges: mst, totalWeight: mstSum };
+};
+
+calcMinimumSpanningTree(5, [
+  [0, 1, 2],
+  [0, 3, 6],
+  [1, 2, 3],
+  [1, 3, 8],
+  [1, 4, 5],
+  [4, 2, 7],
+]);
