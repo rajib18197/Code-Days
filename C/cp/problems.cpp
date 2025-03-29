e822988fff883a953328c52e158098778d4ad606
rajib18197 <rajib18197@gmail.com>
2025-03-29 23:50:01 +0600

    problems related to bst, arrays

--- C/cp/problems.cpp
+++ C/cp/problems.cpp
@@ -22,17 +22,68 @@ int main(){
 
     // cout << totalSum - sum << endl;
 
-    long long n;
+    // long long n;
+    // cin >> n;
+    // cout << n << " ";
+    // while(n != 1){
+    //     if(n % 2 == 0){
+    //         n /= 2;
+    //     }else{
+    //         n *= 3;
+    //         n += 1;
+    //     }
+    //     cout << n << " ";
+    // }
+    int n;
+    int num;
+    vector<int> v;
     cin >> n;
-    cout << n << " ";
-    while(n != 1){
-        if(n % 2 == 0){
-            n /= 2;
-        }else{
-            n *= 3;
-            n += 1;
-        }
-        cout << n << " ";
+    for(int i = 1; i <= n; i++){
+        cin >> num;
+        v.push_back(num);
     }
+
+int count = 0;
+for (int i = 1; i <= v.size(); i++) {
+  int k = i;
+  int final = i % 10;
+  int isSame = true;
+  while (k > 0) {
+    int rem = k % 10;
+    k = k / 10;
+
+    if (final == rem) {
+      final = rem;
+      continue;
+    } else {
+      isSame = false;
+      break;
+    }
+  }
+  if (isSame == false) continue;
+  for (int j = 1; j <= v[i - 1]; j++) {
+    int k = j;
+    int final1 = j % 10;
+    int isSame = true;
+    while (k > 0) {
+      int rem = k % 10;
+      k = k / 10;
+
+      if (final1 == rem) {
+        final1 = rem;
+        continue;
+      } else {
+        isSame = false;
+        break;
+      }
+    }
+    if (isSame == false) continue;
+    if (final == final1) {
+      count++;
+    }
+  }
+}
+cout<< count << endl;
+
 }
 
