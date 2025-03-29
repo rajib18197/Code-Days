296ecc2b284f770de55ce8e25cb1a64d765dd35f
rajib18197 <rajib18197@gmail.com>
2025-03-29 23:33:12 +0600

    problems related to bst, arrays

--- Plain-TS/DSA/dsa.js
+++ Plain-TS/DSA/dsa.js
@@ -1,59 +1,194 @@
-var DisjointSet = /** @class */ (function () {
-    function DisjointSet(length) {
-        this.parent = Array.from({ length: length }, function (_, i) { return i; });
-        this.rank = Array.from({ length: length }, function () { return 1; });
-        this.size = Array.from({ length: length }, function () { return 1; });
+// class DisjointSet {
+//   private parent: number[];
+//   private rank: number[];
+//   private size: number[];
+//   constructor(length: number) {
+//     this.parent = Array.from({ length: length }, (_, i) => i);
+//     this.rank = Array.from({ length: length }, () => 1);
+//     this.size = Array.from({ length: length }, () => 1);
+//   }
+//   findUltimateParent(u: number): number {
+//     if (this.parent[u] === u) {
+//       return u;
+//     }
+//     return (this.parent[u] = this.findUltimateParent(this.parent[u]));
+//   }
+//   unionByRank(u: number, v: number): boolean {
+//     const ult_u: number = this.findUltimateParent(u);
+//     const ult_v: number = this.findUltimateParent(v);
+//     if (ult_u === ult_v) {
+//       return false;
+//     }
+//     if (this.rank[ult_u] === this.rank[ult_v]) {
+//       this.parent[ult_u] = ult_v;
+//       this.rank[ult_v]++;
+//       return true;
+//     }
+//     if (this.rank[ult_u] < this.rank[ult_v]) {
+//       this.parent[ult_u] = ult_v;
+//       return true;
+//     }
+//     if (this.rank[ult_u] > this.rank[ult_v]) {
+//       this.parent[ult_v] = ult_u;
+//       return true;
+//     }
+//     return false;
+//   }
+//   unionBySize(u: number, v: number): boolean {
+//     const ult_u: number = this.findUltimateParent(u);
+//     const ult_v: number = this.findUltimateParent(v);
+//     if (ult_u === ult_v) {
+//       return false;
+//     }
+//     if (this.size[ult_u] === this.size[ult_v]) {
+//       this.parent[ult_u] = ult_v;
+//       this.size[ult_v] += this.size[ult_u];
+//       return true;
+//     }
+//     if (this.size[ult_u] < this.size[ult_v]) {
+//       this.parent[ult_u] = ult_v;
+//       this.size[ult_v] += this.size[ult_u];
+//       return true;
+//     }
+//     if (this.size[ult_u] > this.size[ult_v]) {
+//       this.parent[ult_v] = ult_u;
+//       this.size[ult_u] += this.size[ult_v];
+//       return true;
+//     }
+//     return false;
+//   }
+// }
+// const ds: DisjointSet = new DisjointSet(3);
+// ds.unionByRank(1, 2);
+// // const arr: Array<(number, number)> = [1, 2];
+// let arr: [number, number];
+// arr = [1, 2];
+// for (let i = 1; i <= 10; i++) {
+//   let modified_i = i % 5;
+//   let result = "";
+//   if (i <= 5) {
+//     if (i !== 1) {
+//       for (let k = 1; k < i; k++) {
+//         result = result + " ";
+//       }
+//     }
+//     for (let j = modified_i || 5; j <= 5; j++) {
+//       result = result + j + " ";
+//     }
+//   } else {
+//     if (i !== 10) {
+//       for (let k = 10; k > i; k--) {
+//         result = result + " ";
+//       }
+//     }
+//     let total = modified_i || 5;
+//     for (let j = 1; j <= total; j++) {
+//       result = result + j + " ";
+//     }
+//   }
+//   console.log(result);
+// }
+// class NodeBST<T> {
+//   constructor(
+//     public data: T,
+//     public left: NodeBST<T> | null = null,
+//     public right: NodeBST<T> | null = null
+//   ) {}
+// }
+// const preorderBST = function (arr: number[]) {
+//   const stack1: [number, NodeBST<number> | null, string][] = [];
+//   const stack2: [number, NodeBST<number> | null, string][] = [];
+//   stack1.push([Number.MIN_VALUE, null, ""]);
+//   stack2.push([Number.MAX_VALUE, null, ""]);
+//   let index = 0;
+//   let root = null;
+//   while (index !== arr.length && stack1.length !== 0 && stack2.length !== 0) {
+//     if (arr[index] < stack1[stack1.length - 1][0]) {
+//       return null;
+//     }
+//     while (arr[index] > stack2[stack2.length - 1][0]) {
+//       stack1.pop();
+//       stack2.pop();
+//     }
+//     const lower = stack1.pop();
+//     const upper = stack2.pop();
+//     if (!lower && !upper) {
+//       continue;
+//     }
+//     let newNode = null;
+//     if (lower[0] === Number.MIN_VALUE && upper[0] === Number.MAX_VALUE) {
+//       root = new NodeBST(arr[index]);
+//       newNode = root;
+//     } else {
+//       if (lower[1] !== null) {
+//         const node = lower[1];
+//         // console.log(node, 21);
+//         node[lower[2]] = new NodeBST(arr[index]);
+//         newNode = node[lower[2]];
+//       } else {
+//         const node = upper[1];
+//         // console.log(node, 22);
+//         node[upper[2]] = new NodeBST(arr[index]);
+//         newNode = node[upper[2]];
+//       }
+//     }
+//     stack1.push([arr[index], newNode, "right"]);
+//     stack2.push([upper[0], null, ""]);
+//     stack1.push([lower[0], null, ""]);
+//     stack2.push([arr[index], newNode, "left"]);
+//     index++;
+//   }
+//   return root;
+// };
+// const arr = [10, 5, 1, 7, 40, 50];
+// console.log(preorderBST(arr));
+// const months = [10, 1, 2, 3, 4, 5, 6, 7, 8, 100];
+// const months = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+var months = [
+    73, 8, 55, 26, 97, 48, 37, 47, 35, 55, 5, 17, 62, 2, 60, 23, 99, 73, 34, 75,
+    7, 46, 82, 84, 29, 41, 32, 31, 52, 32,
+];
+var count = 0;
+for (var i = 1; i <= months.length; i++) {
+    var k = i;
+    var final = i % 10;
+    var isSame = true;
+    while (k > 0) {
+        var rem = k % 10;
+        k = Math.floor(k / 10);
+        if (final === rem) {
+            final = rem;
+            continue;
+        }
+        else {
+            isSame = false;
+            break;
+        }
     }
-    DisjointSet.prototype.findUltimateParent = function (u) {
-        if (this.parent[u] === u) {
-            return u;
+    if (isSame === false)
+        continue;
+    for (var j = 1; j <= months[i - 1]; j++) {
+        var k_1 = j;
+        var final1 = j % 10;
+        var isSame_1 = true;
+        while (k_1 > 0) {
+            var rem = k_1 % 10;
+            k_1 = Math.floor(k_1 / 10);
+            if (final1 === rem) {
+                final1 = rem;
+                continue;
+            }
+            else {
+                isSame_1 = false;
+                break;
+            }
         }
-        return (this.parent[u] = this.findUltimateParent(this.parent[u]));
-    };
-    DisjointSet.prototype.unionByRank = function (u, v) {
-        var ult_u = this.findUltimateParent(u);
-        var ult_v = this.findUltimateParent(v);
-        if (ult_u === ult_v) {
-            return false;
+        if (isSame_1 === false)
+            continue;
+        if (final === final1) {
+            console.log(i, j);
+            count++;
         }
-        if (this.rank[ult_u] === this.rank[ult_v]) {
-            this.parent[ult_u] = ult_v;
-            this.rank[ult_v]++;
-            return true;
-        }
-        if (this.rank[ult_u] < this.rank[ult_v]) {
-            this.parent[ult_u] = ult_v;
-            return true;
-        }
-        if (this.rank[ult_u] > this.rank[ult_v]) {
-            this.parent[ult_v] = ult_u;
-            return true;
-        }
-        return false;
-    };
-    DisjointSet.prototype.unionBySize = function (u, v) {
-        var ult_u = this.findUltimateParent(u);
-        var ult_v = this.findUltimateParent(v);
-        if (ult_u === ult_v) {
-            return false;
-        }
-        if (this.size[ult_u] === this.size[ult_v]) {
-            this.parent[ult_u] = ult_v;
-            this.size[ult_v]++;
-            return true;
-        }
-        if (this.size[ult_u] < this.size[ult_v]) {
-            this.parent[ult_u] = ult_v;
-            this.size[ult_v]++;
-            return true;
-        }
-        if (this.size[ult_u] > this.size[ult_v]) {
-            this.parent[ult_v] = ult_u;
-            this.size[ult_u]++;
-            return true;
-        }
-        return false;
-    };
-    return DisjointSet;
-}());
-var ds = new DisjointSet(3);
+    }
+}
+console.log(count);
--- Plain-TS/src/DSA-Practice/bst.ts
+++ Plain-TS/src/DSA-Practice/bst.ts
@@ -1,77 +1,77 @@
-// type PrimitiveProps = {
-//   mode: "primitive";
-// };
+type PrimitiveProps = {
+  mode: "primitive";
+};
 
-// type ReferenceProps<T> = {
-//   mode: "reference";
-//   key: keyof T;
-// };
+type ReferenceProps<T> = {
+  mode: "reference";
+  key: keyof T;
+};
 
-// type InsertNodeProps<T> = (PrimitiveProps | ReferenceProps<T>) & {
-//   node: NodeBST<T> | null;
-//   data: T;
-// };
+type InsertNodeProps<T> = (PrimitiveProps | ReferenceProps<T>) & {
+  node: NodeBST<T> | null;
+  data: T;
+};
 
-// class NodeBST<T> {
-//   constructor(
-//     public data: T,
-//     public left: NodeBST<T> | null = null,
-//     public right: NodeBST<T> | null = null
-//   ) {}
-// }
+class NodeBST<T> {
+  constructor(
+    public data: T,
+    public left: NodeBST<T> | null = null,
+    public right: NodeBST<T> | null = null
+  ) {}
+}
 
-// const insert = function <T>(props: InsertNodeProps<T>) {
-//   if (props.node === null) {
-//     const newNode = new NodeBST<T>(props.data);
-//     return newNode;
-//   }
+const insert = function <T>(props: InsertNodeProps<T>) {
+  if (props.node === null) {
+    const newNode = new NodeBST<T>(props.data);
+    return newNode;
+  }
 
-//   if (props.mode === "primitive") {
-//     if (props.node.data > props.data) {
-//       props.node.left = insert({ ...props, node: props.node.left });
-//     } else {
-//       props.node.right = insert({ ...props, node: props.node.right });
-//     }
+  if (props.mode === "primitive") {
+    if (props.node.data > props.data) {
+      props.node.left = insert({ ...props, node: props.node.left });
+    } else {
+      props.node.right = insert({ ...props, node: props.node.right });
+    }
 
-//     return props.node;
-//   } else {
-//     if (props.node.data[props.key] > props.data[props.key]) {
-//       props.node.left = insert({ ...props, node: props.node.left });
-//     } else {
-//       props.node.right = insert({ ...props, node: props.node.right });
-//     }
+    return props.node;
+  } else {
+    if (props.node.data[props.key] > props.data[props.key]) {
+      props.node.left = insert({ ...props, node: props.node.left });
+    } else {
+      props.node.right = insert({ ...props, node: props.node.right });
+    }
 
-//     return props.node;
-//   }
-// };
+    return props.node;
+  }
+};
 
-// const constructBST = function <T>(
-//   arr: T[],
-//   options: PrimitiveProps | ReferenceProps<T>
-// ) {
-//   let root = null;
+const constructBST = function <T>(
+  arr: T[],
+  options: PrimitiveProps | ReferenceProps<T>
+) {
+  let root = null;
 
-//   for (let i = 0; i < arr.length; i++) {
-//     root = insert({
-//       node: root,
-//       data: arr[i],
-//       ...options,
-//     });
-//   }
-//   return root;
-// };
+  for (let i = 0; i < arr.length; i++) {
+    root = insert({
+      node: root,
+      data: arr[i],
+      ...options,
+    });
+  }
+  return root;
+};
 
-// const inorderTraversal = function <T>(node: NodeBST<T> | null): T[] {
-//   const ans: T[] = [];
-//   if (node === null) {
-//     return ans;
-//   }
+const inorderTraversal = function <T>(node: NodeBST<T> | null): T[] {
+  const ans: T[] = [];
+  if (node === null) {
+    return ans;
+  }
 
-//   const left = inorderTraversal(node.left);
-//   ans.push(node.data);
-//   const right = inorderTraversal(node.right);
-//   return [...left, ...ans, ...right];
-// };
+  const left = inorderTraversal(node.left);
+  ans.push(node.data);
+  const right = inorderTraversal(node.right);
+  return [...left, ...ans, ...right];
+};
 
 // const merge2SortedArr = function <T>(arr1: T[], arr2: T[]) {
 //   const mergedArr = [];
@@ -216,34 +216,161 @@
 //   //   console.log(ans);
 // };
 
-type transformProps<T> = {
-  arr: T[];
-  cb: (element: T, index: number, givenArr: T[]) => T;
-};
-const transform = function <T>(props: transformProps<T>) {
-  const transformArr: T[] = [];
-  for (let i = 0; i < props.arr.length; i++) {
-    const value = props.cb(props.arr[i], i, props.arr);
-    transformArr.push(value);
+// type transformProps<T> = {
+//   arr: T[];
+//   cb: (element: T, index: number, givenArr: T[]) => T;
+// };
+
+// const transform = function <T>(props: transformProps<T>) {
+//   const transformArr: T[] = [];
+//   for (let i = 0; i < props.arr.length; i++) {
+//     const value = props.cb(props.arr[i], i, props.arr);
+//     transformArr.push(value);
+//   }
+//   return transformArr;
+// };
+
+// export const init = function () {
+//   const arr = [1, 2, 3];
+//   // arr.map((el, i, a) => {
+//   //   console.log(el);
+//   //   console.log(i);
+//   //   console.log(a);
+//   // });
+//   const double = transform({
+//     arr,
+//     cb: (el: number, i: number, givenArr: number[]) => {
+//       console.log(el);
+//       console.log(i);
+//       console.log(givenArr);
+//       return el * 2;
+//     },
+//   });
+//   console.log(double);
+// };
+
+const constructBSTFromPreorder = function (
+  arr: number[],
+  index: [number],
+  minValue: number,
+  maxValue: number
+) {
+  if (arr.length === index[0]) {
+    return null;
   }
-  return transformArr;
+
+  const value = arr[index[0]];
+  index[0] += 1;
+  if (value < minValue || value > maxValue) {
+    return null;
+  }
+
+  const node = new NodeBST(value);
+  node.left = constructBSTFromPreorder(arr, index, minValue, node.data);
+  node.right = constructBSTFromPreorder(arr, index, node.data, maxValue);
+  return node;
+};
+
+const constructBSTFromPostorder = function (
+  arr: number[],
+  index: [number],
+  lower: number,
+  upper: number
+) {
+  if (index[0] === arr.length) {
+    return null;
+  }
+
+  const value = arr[index[0]];
+
+  if (value < lower || value > upper) {
+    return null;
+  }
+
+  index[0] = index[0] - 1;
+  const node = new NodeBST(value);
+  node.right = constructBSTFromPostorder(arr, index, node.data, upper);
+  node.left = constructBSTFromPostorder(arr, index, lower, node.data);
+  return node;
+};
+
+const preorderBST = function (arr: number[]) {
+  const stack1: [number, NodeBST<number> | null, "left" | "right" | ""][] = [];
+  const stack2: [number, NodeBST<number> | null, "left" | "right" | ""][] = [];
+  stack1.push([Number.MIN_VALUE, null, ""]);
+  stack2.push([Number.MAX_VALUE, null, ""]);
+
+  let index = 0;
+  let root = null;
+
+  while (index !== arr.length && stack1.length !== 0 && stack2.length !== 0) {
+    if (arr[index] < stack1[stack1.length - 1][0]) {
+      return null;
+    }
+
+    while (arr[index] > stack2[stack2.length - 1][0]) {
+      stack1.pop();
+      stack2.pop();
+    }
+
+    const lower = stack1.pop();
+    const upper = stack2.pop();
+
+    if (!lower && !upper) {
+      continue;
+    }
+
+    let newNode = null;
+    if (lower && upper) {
+      if (lower[0] === Number.MIN_VALUE && upper[0] === Number.MAX_VALUE) {
+        root = new NodeBST(arr[index]);
+        newNode = root;
+      } else {
+        if (lower[1] !== null) {
+          const node = lower[1];
+          // console.log(node, 21);
+          if (lower[2] !== "") {
+            node[lower[2]] = new NodeBST(arr[index]);
+            newNode = node[lower[2]];
+          }
+        } else {
+          const node = upper[1];
+          // console.log(node, 22);
+          if (upper[2] !== "") {
+            node[upper[2]] = new NodeBST(arr[index]);
+            newNode = node[upper[2]];
+          }
+        }
+      }
+
+      stack1.push([arr[index], newNode, "right"]);
+      stack2.push([upper[0], null, ""]);
+
+      stack1.push([lower[0], null, ""]);
+      stack2.push([arr[index], newNode, "left"]);
+
+      index++;
+    }
+  }
+  return root;
 };
 
 export const init = function () {
-  const arr = [1, 2, 3];
-  // arr.map((el, i, a) => {
-  //   console.log(el);
-  //   console.log(i);
-  //   console.log(a);
-  // });
-  const double = transform({
-    arr,
-    cb: (el: number, i: number, givenArr: number[]) => {
-      console.log(el);
-      console.log(i);
-      console.log(givenArr);
-      return el * 2;
-    },
-  });
-  console.log(double);
+  // construct bst from preorder
+  const arr = [10, 5, 1, 7, 40, 50];
+  // const index: [number] = [0];
+  // constructBSTFromPreorder(arr, index, Number.MIN_VALUE, Number.MAX_VALUE);
+  // const arr = [1, 7, 5, 50, 40, 10];
+  // const index: [number] = [arr.length - 1];
+  // constructBSTFromPostorder(arr, index, Number.MIN_VALUE, Number.MAX_VALUE);
+  preorderBST(arr);
+  // construct bst from postorder
+  // pre-order and bst
+  // lowest common anchestor in a bst
+  // Print bst element in a given range
+  // check whether bst contains dead end
+  // common nodes in two bst
+  // sorted ll to balanced bst
+  // merge two bst
+  // fixing two nodes of a bst
 };
