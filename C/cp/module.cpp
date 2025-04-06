3af0a95976dd7af88b011f5f0ca4f372684c1669
rajib18197 <rajib18197@gmail.com>
2025-04-06 14:34:41 +0600

    problems related to string

--- C/cp/module.cpp
+++ C/cp/module.cpp
@@ -247,100 +247,284 @@
 //     return 0;
 // }
 
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// int main(){
+//     string s;
+//     cin >> s;
+
+//     int firstIndex = 0;
+//     int lastIndex = s.size() - 1;
+
+//     int arr[26] = {0};
+
+//     for(int i = 0; i < s.size(); i++){
+//         arr[s[i] - 65]++;
+//     }
+
+//     multimap<int, char, greater<int> >mapCount;
+
+//     for(int i = 0; i < 26; i++){
+//         if(arr[i] == 0) {
+//             continue;
+//         }
+//         char ch = i + 65;
+//         mapCount.insert({arr[i], ch});
+//     }
+
+//     char palindrome[s.size() + 1];
+
+//     for(int i = 0; i < s.size(); i++){
+//         palindrome[i] = '!';
+//     }
+
+//     palindrome[s.size()] = '\0';
+    
+//     int count_of_one = 0;
+//     int isPalidrome = 1;
+//     char lastOdd;
+
+//     for(auto it : mapCount){
+//         if(count_of_one == 1 && it.first % 2 == 1){
+//             isPalidrome = 0;
+//             lastOdd = it.second;
+//             break;
+//         }
+
+//         if(it.first == 1){
+//             palindrome[firstIndex++] = it.second;
+//             lastIndex--;
+//             count_of_one++;
+//             lastOdd = it.second;
+//             continue;
+//         }
+
+//         if(it.first == 2){
+//             palindrome[firstIndex++] = it.second;
+//             palindrome[lastIndex--] = it.second;
+//             continue;
+//         }
+
+//         if(it.first % 2 == 1){
+//             count_of_one++;
+//             lastOdd = it.second;
+//         }
+
+//         int first = it.first / 2;
+
+//         while(first--){
+//             palindrome[firstIndex++] = it.second;
+//         }
+        
+//         int last = it.first / 2;
+
+//         while(last--){
+//             palindrome[lastIndex--] = it.second;
+//         }
+//     }
+//     palindrome[s.size()] = '\0';
+
+//     if(isPalidrome == 1){
+//         for(int i = 0; i < s.size(); i++){
+//             if(palindrome[i] == '!'){
+//                 palindrome[i] = lastOdd;
+//                 cout << palindrome[i];
+//             }else{
+//                 cout << palindrome[i];
+//             }
+//         }
+//     }else{
+//         cout << "NO SOLUTION";
+//     }
+
+//     cout << endl;
+
+//     return 0;
+// }
+
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// int main(){
+//     int t;
+//     cin >> t;
+//     while(t--){
+//         int n;
+//         cin >> n;
+//         string s;
+//         cin >> s;
+
+//         int arr[n];
+
+//         for(int i = 0; i < s.size(); i++){
+//             arr[i] = (s[i] - 97) + 1;
+//         }
+
+//         for(int i = 0; i < s.size(); i++){
+//             cout << arr[i] << " ";
+//         }
+//         cout << endl;
+//         int firstLargest = arr[0];
+//         int secondLargest = -1;
+
+//         for(int i = 0; i < n; i++){
+//             if(arr[i] > firstLargest){
+//                 secondLargest = firstLargest;
+//                 firstLargest = arr[i];
+//             }else if(arr[i] < firstLargest && arr[i] > secondLargest){
+//                 secondLargest = arr[i];
+//             }
+//         }
+
+//         cout << firstLargest << endl;
+//         cout << secondLargest << endl;
+
+//         int deletedChar = firstLargest;
+//         long long num1 = 0;
+//         int c = 0;
+
+//         for(int i = 0; i < n; i++){
+//             if(c == 0 && arr[i] == deletedChar){
+//                 c++;
+//                 continue;
+//             }
+//             num1 = num1 * 10 + arr[i];
+//         }
+
+//         deletedChar = secondLargest;
+//         long long num2 = 0;
+//         c = 0;
+
+//         for(int i = 0; i < n; i++){
+//             if(c == 0 && arr[i] == deletedChar){
+//                 c++;
+//                 continue;
+//             }
+//             num2 = num2 * 10 + arr[i];
+//         }
+        
+//         string modified_s;
+
+//         cout << num1 << " " << num2 << endl;
+//         if(num1 < num2){
+//             c = 0;
+//             for(int i = 0; i < n; i++){
+//                 if(c == 0 && arr[i] == firstLargest){
+//                     c++;
+//                     continue;
+//                 }
+
+//                 char ch = 97 + (arr[i] - 1);
+//                 modified_s.push_back(ch);
+//             }
+//         }else{
+//             c = 0;
+//              for(int i = 0; i < n; i++){
+//                 if(c == 0 && arr[i] == secondLargest){
+//                     c++;
+//                     continue;
+//                 }
+
+//                 char ch = 97 + (arr[i] -1);
+//                 // cout << ch << endl;
+//                 modified_s.push_back(ch);
+//             }
+//         }
+
+//         cout << modified_s << endl;
+//     }
+//     return 0;
+// }
+
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// int main(){
+//     string s;
+//     cin >> s;
+
+//     int lowerFrequency[26] = {0};
+//     int upperFrequency[26] = {0};
+
+//     for(int i = 0; i < s.size(); i++){
+//         if(s[i] >= 'a'){
+//             lowerFrequency[s[i] - 'a']++;
+//         }
+//         if(s[i] >= 'A' && s[i] <= 'Z'){
+//             upperFrequency[s[i] - 'A']++;
+//         }
+//     }
+//     int count = 0;
+//     int isOddExist = 0;
+//     for(int i = 0; i < 26; i++){
+//         if(lowerFrequency[i] % 2 == 0){
+//             count += lowerFrequency[i];
+//         }else{
+//             count += lowerFrequency[i] - 1;
+//             isOddExist = 1;
+//         }
+
+//         if(upperFrequency[i] % 2 == 0){
+//             count += upperFrequency[i];
+//         }else{
+//             count += upperFrequency[i] - 1;
+//             isOddExist = 1;
+//         }
+//     }
+
+//     cout << count + isOddExist << endl;
+    
+//     return 0;
+// }
+
 #include <bits/stdc++.h>
 using namespace std;
 
 int main(){
-    string s;
-    cin >> s;
-
-    int firstIndex = 0;
-    int lastIndex = s.size() - 1;
-
-    int arr[26] = {0};
-
+    string s = "is2 an3 This1 apple4";
+    vector<string> ans(10);
+    string temp;
+    int count = 0;
     for(int i = 0; i < s.size(); i++){
-        arr[s[i] - 65]++;
-    }
-
-    multimap<int, char, greater<int> >mapCount;
-
-    for(int i = 0; i < 26; i++){
-        if(arr[i] == 0) {
-            continue;
-        }
-        char ch = i + 65;
-        mapCount.insert({arr[i], ch});
-    }
-
-    char palindrome[s.size() + 1];
-
-    for(int i = 0; i < s.size(); i++){
-        palindrome[i] = '!';
-    }
-
-    palindrome[s.size()] = '\0';
-    
-    int count_of_one = 0;
-    int isPalidrome = 1;
-    char lastOdd;
-    
-    for(auto it : mapCount){
-        if(count_of_one == 1 && it.first % 2 == 1){
-            isPalidrome = 0;
-            lastOdd = it.second;
-            break;
-        }
-
-        if(it.first == 1){
-            palindrome[firstIndex++] = it.second;
-            lastIndex--;
-            count_of_one++;
-            lastOdd = it.second;
-            continue;
-        }
-
-        if(it.first == 2){
-            palindrome[firstIndex++] = it.second;
-            palindrome[lastIndex--] = it.second;
-            continue;
-        }
-
-        if(it.first % 2 == 1){
-            count_of_one++;
-            lastOdd = it.second;
-        }
-
-        int first = it.first / 2;
-
-        while(first--){
-            palindrome[firstIndex++] = it.second;
-        }
-        
-        int last = it.first / 2;
-
-        while(last--){
-            palindrome[lastIndex--] = it.second;
+        if(s[i] == ' '){
+            cout << temp << endl;
+            int index = temp[temp.size() - 1] - '0';
+            temp.pop_back();
+            ans[index - 1] = temp;
+            temp.clear();
+            count++;
+        }else{
+            temp += s[i];
         }
     }
-    palindrome[s.size()] = '\0';
-
-    if(isPalidrome == 1){
-        for(int i = 0; i < s.size(); i++){
-            if(palindrome[i] == '!'){
-                palindrome[i] = lastOdd;
-                cout << palindrome[i];
-            }else{
-                cout << palindrome[i];
-            }
-        }
-    }else{
-        cout << "NO SOLUTION";
+    if(temp.size() >= 1){
+        int index = temp[temp.size() - 1] - '0';
+        temp.pop_back();
+        ans[index - 1] = temp;
+        temp.clear();
+        count++;
     }
 
-    cout << endl;
+    string str;
+    for(int i = 0; i < count; i++){
+        str += ans[i];
+        if(i < count - 1){
+            str += ' ';
+        }else{
+            str += '.';
+        }
+    }
 
+    cout << str << endl;
+
+    string firstName = "Jonas";
+    string job = "teacher";
+    int birthYear = 1991;
+    int year = 2037;
+
+    string sentence = "I'm " + firstName + ", a 30 "  + "year old " + job + '!';
+
+    cout << sentence << endl;
     return 0;
 }
-
-    
